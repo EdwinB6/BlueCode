d746b94f6d57f9215dbc3d62e45809d7b082a598
EdwinB5 <edwinbecerra2001@gmail.com>
2023-08-26 15:11:00 -0500

    Working on loading pages content

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,26 @@
-console.log('Works');
+$(document).ready(function() {
+    $(".load-button").click(function() {
+      var content = $(this).data("content");
+      loadContent(content);
+    });
+
+    function loadContent(contentType) {
+      var contentURL = "";
+
+      switch (contentType) {
+        case "about":
+          contentURL = "pages/about.html";
+          break;
+        case "contact":
+          contentURL = "pages/contact.html";
+          break;
+
+        default:
+          break;
+      }
+
+      if (contentURL !== "") {
+        $("#page-content").load(contentURL);
+      }
+    }
+});
