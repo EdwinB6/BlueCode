433fb349c0829c05d516dbbf38fa7e7e58c680d0
EdwinB5 <edwinbecerra2001@gmail.com>
2023-08-31 04:00:29 -0500

    Working on web server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,24 @@
 "use strict";
-console.log("Work in progress");
+
+import * as dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+import path from 'path';
+
+dotenv.config();
+
+// Starting app
+const app: Express = express();
+const port: number = 8000;
+
+const staticPath: string = path.join(__dirname, 'public');
+
+app.use(express.static(staticPath));
+
+app.use((req: Request, res: Response) => {
+    res.status(404);
+    res.send('<h1>Error 404 not found</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`App listening on ${port}`);
+});
